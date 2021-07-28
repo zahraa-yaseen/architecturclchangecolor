b88cfb14edf7335c284d483dde544c4d9e7adb71
zahraayaseen97 <82576134+zahraayaseen97@users.noreply.github.com>
2021-07-28 19:04:25 +0300

    addfirst

---
+++ js.js
@@ -0,0 +1,21 @@
+
+function myfun(){
+    document.getElementById("contanire").style.background="black";
+    document.body.style.color="white";
+    document.getElementById("contanire").style.color="white";
+    document.getElementById("contanire").style.background="black";
+    document.body.style.background="black";
+
+}
+function myfun2(){
+    document.getElementById("contanire").style.background="crimson";
+   document.body.style.background="crimson";
+  
+
+}
+function myfun3(){
+    document.getElementById("contanire").style.background="darkgoldenrod";
+    document.body.style.background="darkgoldenrod";
+  
+
+}
